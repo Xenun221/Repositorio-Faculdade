4b952cb9494d0ba575c6487c29ad5d40b44a7c3f
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-06-23 15:59:38 -0300

    Javascript Conceitos e Uso do console
    Aula de Introdução ao Javascript

---
+++ JavaScript/JavaScriptConceitoEusoDoConsole.js
@@ -0,0 +1,17 @@
+// Comentario de uma unica linha
+/*
+Comentario de multiplas linhas
+*/
+
+/*
+    A maioria da nossa execução vai ser exibida no nosso navegador para isso vamos usar o console.
+    Console mostra os erros tambem 
+    Não e necessario o uso do ;
+    posso usar aspas simples, aspas duplas e Craze
+*/
+
+//Usando Console
+console.log("Usando o Console")
+console.log("Olá eu sou o Console")
+console.log('Usando aspas Simples');
+console.log(`Usando as Crazes`);
