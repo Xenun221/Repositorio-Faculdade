41285fa58ab3c6ce028b523fa27781374eeecb74
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-06-23 18:45:31 -0300

    JavaScript Operadores Aritméticos
    Como utilizar operadores Aritméticos

---
+++ JavaScript/JavaScriptOperadoresAritimeticos.js
@@ -0,0 +1,39 @@
+/*
++ Soma
+- Subitração
+* Multiplicação
+/ Divisão
+% Modulo
+** Potencia
+*/
+
+let n1 = 50
+let n2 = 10
+let resultado
+
+console.log(`Primeiro Numero ${n1}`)
+console.log(`Segundo Numero ${n2}`)
+
+// Adição
+resultado = n1 +n2
+console.log(`${n1} + ${n2} =  ${resultado}`)
+
+// Subtração
+resultado = n1 - n2
+console.log(`${n1} + ${n2} =  ${resultado}`)
+
+// Divisão
+resultado = n1 /n2
+console.log(`${n1} + ${n2} =  ${resultado}`)
+
+// Multiplicação
+resultado = n1 * n2
+console.log(`${n1} + ${n2} =  ${resultado}`)
+
+// Modulo(Resto da divisão)
+resultado = n1 % n2
+console.log(`${n1} + ${n2} =  ${resultado}`)
+
+// Potencia
+resultado = n1 ** n2
+console.log(`${n1} + ${n2} =  ${resultado}`)
