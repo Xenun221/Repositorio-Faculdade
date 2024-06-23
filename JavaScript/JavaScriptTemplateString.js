b4f75c2657a921bb96c371b2ee31ed24411309a1
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-06-23 18:17:57 -0300

    JavaScript Template String
    Aula de como utilizar o Template String

---
+++ JavaScript/JavaScriptTemplateString.js
@@ -0,0 +1,14 @@
+let nome = "Wade Wilson"
+let poder = "Assasino"
+let estaTrampando = false
+
+console.log(nome);
+console.log(poder);
+console.log(estaTrampando);
+
+// Exibindo tempalte
+/*Iniciar com craze 
+    ${Variavel} para declarar
+*/
+console.log(`Nome: ${nome}, ele tem como poder ${poder}, 
+    ele esta trampando ?${estaTrampando}`);
